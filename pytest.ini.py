b8acc068e5c55c85c2e2fd690893610c33853abf
Gheeraert Tony <113168672+Gheeraert@users.noreply.github.com>
2026-06-16 14:10:30 +0200

    ajout d'un pytest ini

---
+++ pytest.ini.py
@@ -0,0 +1,12 @@
+[pytest]
+testpaths =
+    tests
+
+norecursedirs =
+    .git
+    .venv
+    __pycache__
+    tests/_runtime
+    tests/_pytest_tmp_site_builder
+    _runtime
+    _pytest_tmp_site_builder
